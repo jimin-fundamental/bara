8f86e0412c91946912ac02d788b36ad17a2fe74d
Kareeenn <102471138+Kareeenn@users.noreply.github.com>
2023-11-16 21:33:32 +0900

    start

---
+++ src/main/java/egeg/BARA/BaraApplication.java
@@ -0,0 +1,13 @@
+package egeg.BARA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/egeg/BARA/BaraApplicationTests.java
@@ -0,0 +1,13 @@
+package egeg.BARA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
