ce3670470de60671999a97ec567aa42fa43a41a0
Kareeenn <102471138+Kareeenn@users.noreply.github.com>
2023-11-21 18:04:08 +0900

    화면 css 적용 안되는 문제 해결중

--- src/main/java/egeg/BARA/BaraApplication.java
+++ src/main/java/egeg/BARA/BaraApplication.java
@@ -2,8 +2,9 @@ package egeg.BARA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BaraApplication {
 
 	public static void main(String[] args) {
