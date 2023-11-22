94a9f825684e20759d1f1fa11c8b7f431ee35e3f
sforseohn <sforseohn@ewhain.net>
2023-11-23 00:33:29 +0900

    Feat: room repository

---
+++ src/main/java/egeg/BARA/respository/RoomRepository.java
@@ -0,0 +1,11 @@
+package egeg.BARA.respository;
+
+import egeg.BARA.domain.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    Room findById(int id);
+    List<Room> findByPlace(String place);
+}
